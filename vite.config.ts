402cf6ba138826c8ccaea326a9169b0ed40308cf
TomCioo <97665030+TomCioo@users.noreply.github.com>
2026-05-28 21:18:03 +0200

    ffa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-import { tanstackStart } from "@tanstack/react-start/vite"
+import tsconfigPaths from "vite-tsconfig-paths"
+import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
   plugins: [
     react(),
-    tanstackStart(),
+    tsconfigPaths(),
+    tailwindcss(),
   ],
 })
